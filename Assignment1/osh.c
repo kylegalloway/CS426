0f9321bfae8a31ceaa208bb1ed49c233e2ddf5bc
Kyle <ckgalloway@crimson.ua.edu>
2016-02-11 20:25:03 -0600

    commit

--- Assignment1/osh.c
+++ Assignment1/osh.c
@@ -9,6 +9,14 @@
 
 #define MIN(a,b)(a < b ? a : b) /* Finds the min of 2 inputs */
 
+
+struct command
+{
+    int id;
+    char* tokens[MAX_LINE / 2 + 1];
+    int background;
+};
+
 /*
     Trims the character from the string if it is the last character.
     Returns a boolean based on whether the character was present.
@@ -30,23 +38,29 @@ trim_trailing_match (char* s, char ch)
     return 1;
 }
 
-char** tokenize(const char* input)
+/* TODO: Description */
+char**
+tokenize(const char* input)
 {
     char* str = strdup(input);
     int count = 0;
     int capacity = 10;
     char** result = malloc(capacity*sizeof(*result));
-
     char* tok=strtok(str," ");
 
     while(1)
     {
         if (count >= capacity)
+        {
             result = realloc(result, (capacity*=2)*sizeof(*result));
+        }
 
         result[count++] = tok? strdup(tok) : tok;
 
-        if (!tok) break;
+        if (!tok)
+        {
+            break;
+        }
 
         tok=strtok(NULL," ");
     }
@@ -55,13 +69,36 @@ char** tokenize(const char* input)
     return result;
 }
 
-struct command
+/* TODO: Description */
+int
+run_command(struct command* args, int place, int background)
 {
-    int id;
-    char* tokens[MAX_LINE / 2 + 1];
-    int background;
-};
+    /* fork a child process */
+    pid_t pid = fork();
 
+    /* error occurred */
+    if (pid < 0) {
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    /* if child process */
+    else if (pid == 0)
+    {
+        execvp(args[place].tokens[0], args[place].tokens);
+    }
+    /* else parent process*/
+    else
+    {
+        if (background) /* If run in background */
+        {
+            wait(NULL); /* parent will wait for the child to complete */
+        }
+    }
+
+    return 0;
+}
+
+/* TODO: Description */
 int
 main(void)
 {
@@ -85,73 +122,27 @@ main(void)
 
         memcpy(args, tokenize(buffer), sizeof(args));
 
-        // if(args[0][0] == '!')
-        // {
-        //     char second = args[0][1];
-        //     if (second == '!')
-        //     {
-        //         background = history[0].background;
-        //         /* fork a child process */
-        //         pid_t pid = fork();
-
-        //         /* error occurred */
-        //         if (pid < 0) {
-        //             fprintf(stderr, "Fork Failed");
-        //             return 1;
-        //         }
-        //         /* if child process */
-        //         else if (pid == 0)
-        //         {
-        //             execvp(history[0].tokens[0], history[0].tokens);
-        //         }
-        //         /* else parent process*/
-        //         else
-        //         {
-        //             if (background) /* If run in background */
-        //             {
-        //                 /* parent will wait for the child to complete */
-        //                 wait(NULL);
-        //             }
-        //         }
-        //     }
-        //     else if (atoi(second) > 0 && atoi(second) < 11)
-        //         {
-        //         background = history[atoi(second)].background;
-        //         /* fork a child process */
-        //         pid_t pid = fork();
-
-        //         /* error occurred */
-        //         if (pid < 0) {
-        //             fprintf(stderr, "Fork Failed");
-        //             return 1;
-        //         }
-        //         /* if child process */
-        //         else if (pid == 0)
-        //         {
-        //             execvp(history[atoi(second)].tokens[0], history[atoi(second)].tokens);
-        //         }
-        //         /* else parent process*/
-        //         else
-        //         {
-        //             if (background) /* If run in background */
-        //             {
-        //                 /* parent will wait for the child to complete */
-        //                 wait(NULL);
-        //             }
-        //         }
-        //     }
-        //     else
-        //     {
-        //         printf("That command does not exist.\n");
-        //         /* Resets args */
-        //         memset(args, '\0', sizeof(args));
-
-        //         /* Resets argc */
-        //         argc = 0;
-        //         continue;
-        //     }
-        // }
-        /*else*/ if (strcmp(args[0], "history") != 0)
+        if(args[0][0] == '!')
+        {
+            char second = args[0][1];
+            int second_int = second - '0';
+            if (second == '!')
+            {
+                background = history[command_count % 10 - 1].background;
+                run_command(history, (command_count % 10) - 1, background);
+            }
+            else if (second_int > 0 && second_int < 11)
+            {
+                int num = second_int - 1;
+                background = history[num].background;
+                run_command(history, num, background);
+            }
+            else
+            {
+                printf("No such command in history.\n");
+            }
+        }
+        else if (strcmp(args[0], "history") != 0)
         {
             int spot = command_count % 10;
             history[spot].id = command_count;
@@ -159,35 +150,14 @@ main(void)
             history[spot].background = background;
             command_count++;
 
-            /* fork a child process */
-            pid_t pid = fork();
-
-            /* error occurred */
-            if (pid < 0) {
-                fprintf(stderr, "Fork Failed");
-                return 1;
-            }
-            /* if child process */
-            else if (pid == 0)
-            {
-                execvp(args[0], args);
-            }
-            /* else parent process*/
-            else
-            {
-                if (background) /* If run in background */
-                {
-                    /* parent will wait for the child to complete */
-                    wait(NULL);
-                }
-            }
+            run_command(history, spot, background);
         }
         else
         {
             int i = 0;
             while (i < 10 && command_count - i > 0)
             {
-                printf("%d\t%s\t%d\n", history[i].id, history[i].tokens[0], history[i].background);
+                printf("%d\t%s\n", MIN(10, command_count) - history[i].id, history[i].tokens[0]);
                 ++i;
             }
         }
