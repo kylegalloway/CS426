3f2632f8861bc6af95ea6a42e4a503317b1e1033
Kyle Galloway <kyle.galloway.1993@gmail.com>
2016-04-17 21:17:31 -0500

    update

--- Assignment5/assign5.c
+++ Assignment5/assign5.c
@@ -16,6 +16,12 @@ typedef struct {
   int valid;
 } Page;
 
+typedef struct {
+  int pageNumber;
+  int frameNumber;
+  int valid;
+} TLBEntry;
+
 int main(int argc, char const *argv[])
 {
     if (argc != 2) {
@@ -46,20 +52,25 @@ int main(int argc, char const *argv[])
     const Page defaultPage = { .valid = 0 };
     /* Initialize the page table using the defaultPage. */
     Page pageTable[PAGE_TABLE_SIZE];
-    for (int i = 0; i < PAGE_TABLE_SIZE; i++)
+    for (int i = 0; i < PAGE_TABLE_SIZE; ++i)
     {
         pageTable[i] = defaultPage;
     }
 
     /* Initialize an array to keep track of free frames. 0 = free. */
-    int freeFrames[FRAME_COUNT];
-    for (int i = 0; i < FRAME_COUNT; i++)
-    {
-        freeFrames[i] = 0;
-    }
+    // int freeFrames[FRAME_COUNT];
+    // for (int i = 0; i < FRAME_COUNT; ++i)
+    // {
+    //     freeFrames[i] = 0;
+    // }
 
     /* Next frame to be used. (For FIFO). */
     int nextFrame = 0;
+
+    /* Set up translation look-aside buffer as FIFO array */
+    TLBEntry TLB[TLB_SIZE];
+    int TLBHead = 0;
+    int CurrTLBSize = 0;
 
     /* Read the logical addresses and run the paging system. */
     char line[256]; /* Buffer for lines of address file. */
@@ -78,58 +89,97 @@ int main(int argc, char const *argv[])
         /* -1 indicates an unknown page number. */
         int frameNumber = -1;
 
-        /* Check if the page is in the pageTable. */
-        if (pageTable[pageNumber].valid)
+        /* Check if the TLB is empty. */
+        if (CurrTLBSize > 0)
         {
-            frameNumber = pageTable[pageNumber].frameNumber;
-        }
-        /* If not, bring it into memory. */
-        else
-        {
-            /* Record the page fault. */
-            ++totalPageFaults;
-            /* Jump to the next frame to be used. */
-            frameNumber = nextFrame;
-            /* If that frame is free, */
-            if (freeFrames[frameNumber] != 0)
+            /* If not empty, walk through and see if the page is in it. */
+            for (int i = 0; i < CurrTLBSize; ++i)
             {
-                /* Make all references to that frameNumber invalid. */
-                for (int i = 0; i < PAGE_TABLE_SIZE; ++i)
+                if (TLB[i].valid && TLB[i].pageNumber == pageNumber)
                 {
-                    /* Find the valid reference to the frameNumber. */
-                    if (pageTable[i].valid && pageTable[i].frameNumber == frameNumber)
-                    {
-                        /* Then make it invalid and break out of the loop. */
-                        pageTable[i].valid = 0;
-                        break;
-                    }
+                  ++totalTLBHits;
+                  frameNumber = TLB[i].frameNumber;
                 }
             }
+        }
+        /* If the frameNumber is still -1, the TLB missed. */
+        if (frameNumber == -1)
+        {
+            /* Check if the page is in the pageTable. */
+            if (pageTable[pageNumber].valid)
+            {
+                frameNumber = pageTable[pageNumber].frameNumber;
+            }
+            /* If not, bring it into memory. */
             else
             {
-                freeFrames[frameNumber] = 1;
+                /* Record the page fault. */
+                ++totalPageFaults;
+                /* Jump to the next frame to be used. */
+                frameNumber = nextFrame;
+                /* If that frame is free, */
+                if (freeFrames[frameNumber] != 0)
+                {
+                    /* Make all references to that frameNumber invalid. */
+                    for (int i = 0; i < PAGE_TABLE_SIZE; ++i)
+                    {
+                        /* Find the valid reference to the frameNumber. */
+                        if (pageTable[i].valid && pageTable[i].frameNumber == frameNumber)
+                        {
+                            /* Then make it invalid and break out of the loop. */
+                            pageTable[i].valid = 0;
+                            break;
+                        }
+                    }
+                    /* Make all references to that frameNumber invalid. */
+                    for (int i = 0; i < CurrTLBSize; ++i)
+                    {
+                        /* Find the valid reference to the frameNumber. */
+                        if (TLB[i].valid && TLB[i].frameNumber == frameNumber)
+                        {
+                            /* Then make it invalid and break out of the loop. */
+                            TLB[i].valid = 0;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    freeFrames[frameNumber] = 1;
+                }
+
+                /* Copy the page from the backing store into physical memory. */
+                /* SEEK_SET makes fseek relative to the beginning of the file. */
+                fseek(backingFile, pageNumber * PAGE_SIZE, SEEK_SET);
+                fread(&physicalMemory[frameNumber * PAGE_SIZE], 1, PAGE_SIZE, backingFile);
+
+                /* Update the page table. */
+                pageTable[pageNumber].frameNumber = frameNumber;
+                pageTable[pageNumber].valid = 1;
+
+                /* Update the TLB. */
+                TLBEntry newTLBEntry = {
+                                    .pageNumber = pageNumber,
+                                    .frameNumber = frameNumber,
+                                    .valid = 1
+                                };
+                TLB[TLBHead] = newTLBEntry;
+
+                /* Update CurrTLBSize, TLBHead, and nextFrame. */
+                TLBHead = ++TLBHead % TLB_SIZE; // FIFO TLB entry replacement
+                if (CurrTLBSize < TLB_SIZE) ++CurrTLBSize;
+
+                /* Set the nextFrame to use FIFO replacement. */
+                nextFrame = ++nextFrame % FRAME_COUNT;
             }
 
-            /* Copy the page from the backing store into physical memory. */
-            /* SEEK_SET makes fseek relative to the beginning of the file. */
-            fseek(backingFile, pageNumber * PAGE_SIZE, SEEK_SET);
-            fread(&physicalMemory[frameNumber * PAGE_SIZE], 1, PAGE_SIZE, backingFile);
-
-            /* Update the page table. */
-            pageTable[pageNumber].frameNumber = frameNumber;
-            pageTable[pageNumber].valid = 1;
-
-
-            /* Set the nextFrame to use FIFO replacement. */
-            nextFrame = ++nextFrame % FRAME_COUNT;
+            /* Read the value from memory and print the output. */
+            int physicalAddress = (frameNumber * PAGE_SIZE) + pageOffset;
+            /* TODO: Value doesn't work, need to format as signed byte */
+            char value = physicalMemory[physicalAddress];
+            printf("Virtual address: %d Physical address: %d Value: %d\n", logicalAddr, physicalAddress, value);
+            totalMemoryAccesses++;
         }
-
-        /* Read the value from memory and print the output. */
-        int physicalAddress = (frameNumber * PAGE_SIZE) + pageOffset;
-        /* TODO: Value doesn't work, need to format as signed byte */
-        char value = physicalMemory[physicalAddress];
-        printf("Virtual address: %d Physical address: %d Value: %d\n", logicalAddr, physicalAddress, value);
-        totalMemoryAccesses++;
     }
 
     fclose(addressesFile);
